0d7e9cd8f9d530a0f1443033ba7d7740144c3ed7
Filip Waligórski <filip.waligorski@student.put.poznan.pl>
2016-10-07 18:21:00 +0200

    manual commit

---
+++ testProj/testProj/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace testProj
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Hello World!");
+			Console.WriteLine ("Hello World!");
+		}
+	}
+}
